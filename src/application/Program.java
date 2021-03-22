856b58bb2fa0d6c7984361f291035199fc1eb041
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-22 20:56:31 -0300

    Trabalhando com Comparator (Usando na forma lambda)

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,15 +18,24 @@ public class Program {
 	
 		//list.sort(new MyComparator());
 		
-		// Definindo um objecto que recebe uma class anonima
-		Comparator<Product> comp = new Comparator<Product>() {
-			@Override
-			public int compare(Product p1, Product p2) {				
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
-			
+		/* 
+		 * 		Definindo um objecto que recebe uma class anonima
+			Comparator<Product> comp = new Comparator<Product>() {
+				@Override
+				public int compare(Product p1, Product p2) {				
+					return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+				}
+				
+			};
+		*/
+		
+		// Usando expressões mais enxuto, ou seja expressões lambda
+		// (p1, p2) -> : é assim que se define de forma enxuta uma função anonima
+		Comparator<Product> comp02 = (p1, p2) ->{
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
-		list.sort(comp);
+		
+		list.sort(comp02);
 		
 		for(Product p : list) {
 			System.out.println(p);
